b38f196931443572483efbda688daa4994e6078a
Tara <tara.petric@gmail.com>
2020-08-05 12:41:42 +0200

    run thread

--- run_all_new.py
+++ run_all_new.py
@@ -8,6 +8,7 @@ import threading
 for NA in [CureAgent,MutationBlindNanoAgent,InfiniteFixedCureAgent]:
     print (NA.__name__)
     t = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,modifier_fraction,True))
+    t.start()
     for modifier_fraction in [0.2,0.3,0.4,0.5]:
         tt = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0,modifier_fraction,False))
         tf = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,modifier_fraction,False))
