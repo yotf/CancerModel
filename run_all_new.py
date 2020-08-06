1a20923d6b74432b4bac643300ae4d21e0287257
Tara <tara.petric@gmail.com>
2020-08-06 22:20:57 +0200

    multiprocess umesto multithread

--- run_all_new.py
+++ run_all_new.py
@@ -2,16 +2,16 @@ from rob_ev import *
 import pandas as pd
 from model import *
 from parameters import *
-import threading
+from multiprocessing import Process
 
 
 for NA in [CureAgent,MutationBlindNanoAgent,InfiniteFixedCureAgent]:
     print (NA.__name__)
-    t = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,0.1,True))
+    t = Process(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,0.1,True))
     t.start()
     for modifier_fraction in [0.2,0.3,0.4,0.5]:
-        tt = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0,modifier_fraction,False))
-        tf = threading.Thread(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,modifier_fraction,False))
+        tt = Process(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0,modifier_fraction,False))
+        tf = Process(target=run_model_and_get_results,args=(SAMPLE_SIZE,NA,STEPS,None,False,0.001,modifier_fraction,False))
         tt.start()
         tf.start()
 
