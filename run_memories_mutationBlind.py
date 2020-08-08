be89466949db86b93f92fcfc87a08f535fa30e3e
Tara <tara.petric@gmail.com>
2020-08-08 16:09:06 +0200

    memories for mutationblind

--- run_memories_mutationBlind.py
+++ run_memories_mutationBlind.py
@@ -7,4 +7,4 @@ SAMPLE_SIZE = 200
 for i in range(10):
     rez = run_model_and_get_results(SAMPLE_SIZE,MutationBlindNanoAgent,STEPS,agent_memory_type=i)
     for s,r in enumerate(rez):
-        r.to_csv("rez_%s-Memory%s-%sSTEPS-%s.csv" %(CureAgent.__name__,i,STEPS,s))
+        r.to_csv("rez_%s-Memory%s-%sSTEPS-%s.csv" %(MutationBlindNanoAgent.__name__,i,STEPS,s))
